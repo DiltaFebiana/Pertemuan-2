c3d3255705d853863c76926b1cabbf9de25a62a3
DiltaFebiana <febianadilta@gmail.com>
2023-03-06 09:59:59 +0700

    Final

---
+++ main1.js
@@ -0,0 +1,5 @@
+// Mengimpor semua ekspor dari "myModule.js" ke dalam satu objek dengan nama "myModule"
+import * as myModule from './myModule.js';
+
+console.log(myModule.foo); // Output: "Hello"
+console.log(myModule.bar); // Output: "World"
